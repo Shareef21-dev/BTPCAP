a78d0c1a4e5f77081fa2729241dfcbeb0480f3e4
shaik Shareef <shaiksharru798@gmail.com>
2026-09-24 14:20:51 +0000

    rest protocol

--- productshop/srv/service.cds
+++ productshop/srv/service.cds
@@ -1,3 +1,8 @@
+@protocol:'rest'
 service product {
-    function printhelloworld(input:String) returns String
+    function printhelloworld(input:String) returns String;
+
+    @open
+    type object {};
+    function Myfunction(categeory:Integer) returns object
 }
--- productshop/srv/service.js
+++ productshop/srv/service.js
@@ -6,6 +6,18 @@ module.exports= cds.service.impl(async function (srv) {
         console.log(req.data.input)
         return `${req.data.input} World`
     })
+    srv.on('Myfunction',req=>{
+        let result={}
+        if(req.data.categeory==1){
+            result.product = 'BMW'
+            result.price = "20000"
+        }else{
+            result.product = 'Audi'
+            result.discount = '20%'
+        }
+        console.log(result)
+        return result
+    })
     
 })
 
